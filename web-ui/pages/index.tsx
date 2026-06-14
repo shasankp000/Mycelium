455901d9535ac002996bdfb898252c722e510e51
Shasank Prasad <shasankp.14@gmail.com>
2026-06-14 19:55:39 +0530

    Defaulted router behaviour to the landing page always

--- web-ui/pages/index.tsx
+++ web-ui/pages/index.tsx
@@ -1,17 +1,5 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
 import LandingPage from '../components/LandingPage';
 
 export default function Page() {
-  const router = useRouter();
-  const [ready, setReady] = useState(false);
-
-  useEffect(() => {
-    const hasVisited = localStorage.getItem('mycelium-visited');
-    setReady(true);
-    if (hasVisited) router.replace('/app');
-  }, []);
-
-  if (!ready) return null;
   return <LandingPage />;
 }
